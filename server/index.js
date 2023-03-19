60eb5c50e9468b0f91fe882d104cb924af26ec88
jonathan <jonathan>
2023-03-19 19:33:51 -0300

    process.env.PORT

--- server/index.js
+++ server/index.js
@@ -1,20 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const route  = require('./routes/routes')
+const route = require("./routes/routes");
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
-app.use(route)
-const port = 3010;
+app.use(route);
+const port = process.env.PORT || 3010;
 app.listen(port, () => {
-  console.log(`server on port 3010`)
-
+  console.log(`server on port 3010`);
 });
-
-
-
-
-
